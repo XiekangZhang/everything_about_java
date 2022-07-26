82b80dc64ae9da62271c0f14af5205fa8ff69fda
XiekangZhang <zhangxiekang@gmail.com>
2022-07-26 23:51:57 +0200

    finishing preparation

---
+++ java_basic/src/main/java/de/xiekang/basic/MyArray.java
@@ -0,0 +1,18 @@
+package de.xiekang.basic;
+
+
+/**
+ * TODO:
+ * 1. Create a two dimensional array with number
+ * 2. Add two methods to sum the all columns and rows
+ */
+public class MyArray {
+
+    public int[] sumRow(int[][] a) {
+        return null;
+    }
+
+    public int[] sumColumn(int[][] a) {
+        return null;
+    }
+}
---
+++ java_basic/src/main/java/de/xiekang/basic/README.md
@@ -0,0 +1,6 @@
+# Basic Java Training
+
+1. Starting with Java Class Diagram
+2. Java primitive Type: Be careful! Bears shouldn't ingest large furry dogs.
+3. if else, do while, while, for
+4. Java Object Type: Arrays
